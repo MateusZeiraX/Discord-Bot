5ae3fdd0f8dee9df02950eddb690b2b1bafcf721
mateusdev <73514780+mateuslenda@users.noreply.github.com>
2021-09-07 01:18:41 -0300

    Delete plugins directory

--- plugins/socket.client.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueSocketIO from 'vue-socket.io'
-import io from 'socket.io-client';
- 
-export default function () {
- Vue.use(new VueSocketIO({
-   connection: io()
- }))
-}
